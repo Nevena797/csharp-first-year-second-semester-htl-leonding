508704c85a2478be0c7e6e47d1f09330833f8253
Nevena <nevena.rogova@gmx.at>
2025-03-24 21:54:07 +0200

    Update 04.siebDesEratosthenes3.cs

--- 04.siebDesEratosthenes3.cs
+++ 04.siebDesEratosthenes3.cs
@@ -1,10 +1,41 @@
-﻿namespace _04.siebDesEratosthenes3
+using System;
+
+class Program
 {
-    internal class Program
+    static bool[] SiebDesEratosthenes(int maxZahl)
     {
-        static void Main(string[] args)
+        bool[] istPrim = new bool[maxZahl + 1];
+        for (int i = 2; i <= maxZahl; i++) istPrim[i] = true;
+
+        int maxPrüfung = (int)Math.Sqrt(maxZahl);
+        for (int i = 2; i <= maxPrüfung; i++)
         {
-            Console.WriteLine("Hello, World!");
+            if (istPrim[i])
+            {
+                for (int multiplikator = 2; (multiplikator * i) <= maxZahl; multiplikator++)
+                {
+                    istPrim[multiplikator * i] = false;
+                }
+            }
         }
+        return istPrim;
+    }
+
+    static void Main()
+    {
+        Console.Write("Bis zu welcher Zahl wollen Sie Primzahlen ausgeben? ");
+        int zahl = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"\nPrimzahlen von 1 - {zahl}:");
+
+        bool[] primzahlen = SiebDesEratosthenes(zahl);
+        for (int i = 2; i <= zahl; i++)
+        {
+            if (primzahlen[i])
+                Console.WriteLine(i);
+        }
+
+        Console.WriteLine("\n<Eingabetaste für ENDE>");
+        Console.ReadLine();
     }
 }
