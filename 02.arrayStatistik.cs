48b0d76b77024ae6966931722e76f7b00ebe22af
Nevena <nevena.rogova@gmx.at>
2025-03-24 10:33:04 +0200

    Update 02.arrayStatistik.cs

--- 02.arrayStatistik.cs
+++ 02.arrayStatistik.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ArrayStatistic
 {
@@ -9,28 +9,28 @@ namespace ArrayStatistic
             Console.WriteLine("Array Statistic");
             Console.WriteLine("**********************");
 
-
+            // 1. Anzahl der Zahlen eingeben
             Console.Write("Please enter the count of numbers: ");
             int count = int.Parse(Console.ReadLine());
 
-
+            // 2. Array für die Zahlen initialisieren
             int[] numbers = new int[count];
 
-
+            // 3. Eingabe der Zahlen
             for (int i = 0; i < count; i++)
             {
                 Console.Write($"Please enter {i + 1}. number: ");
                 numbers[i] = int.Parse(Console.ReadLine());
             }
 
-
+            // 4. Eingabe der unteren und oberen Grenze
             Console.Write("Please enter lower bound: ");
             int lowerBound = int.Parse(Console.ReadLine());
 
             Console.Write("Please enter upper bound: ");
             int upperBound = int.Parse(Console.ReadLine());
 
-
+            // 5. Berechnung der Zahlen im Bereich
             int countInRange = 0;
             foreach (int number in numbers)
             {
@@ -40,6 +40,7 @@ namespace ArrayStatistic
                 }
             }
 
+            // 6. Ausgabe des Ergebnisses
             Console.WriteLine($"{countInRange} numbers are in the range of [{lowerBound}..{upperBound}]");
             Console.WriteLine("**********************");
         }
