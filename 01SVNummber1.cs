4d5c7b10c5eb0798f513482c5b5c23ed00c43381
Nevena <nevena.rogova@gmx.at>
2025-03-24 16:09:19 +0200

    Add files via upload

---
+++ 01SVNummber1.cs
@@ -0,0 +1,10 @@
+﻿namespace _01.SVNumber1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
