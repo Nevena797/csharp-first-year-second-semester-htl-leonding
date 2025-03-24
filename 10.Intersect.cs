10baf3b2fbda7879ff23a2ab79b01d3d1d4f4a7a
Nevena <nevena.rogova@gmx.at>
2025-03-24 17:35:56 +0200

    Add files via upload

---
+++ 10.Intersect.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbersA = { 1, 3, 5, 7, 9 };
+        int[] numbersB = { 0, 1, 2, 3 };
+
+        // Aufruf der Intersect-Methode
+        int[] result = IntersectTools.Intersect(numbersA, numbersB);
+
+        // Ausgabe des Ergebnisses
+        Console.WriteLine("Gemeinsame Zahlen: " + string.Join(", ", result));
+    }
+    public static class IntersectTools
+    {
+        public static int[] Intersect(int[] array1, int[] array2)
+        {
+            // Entfernt Duplikate und bestimmt die gemeinsamen Elemente zwischen beiden Arrays
+            return array1.Distinct().Intersect(array2.Distinct()).ToArray();
+        }
+    }
+}
