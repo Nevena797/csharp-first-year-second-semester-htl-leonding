f6bfcc17bb361c9f4edec30ced854c533af4171f
Nevena <nevena.rogova@gmx.at>
2025-03-25 09:49:21 +0200

    Add files via upload

---
+++ lottoQuicktip3.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace LottoQuickTip
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Lotto-Quicktip:");
+            Console.WriteLine("****************");
+
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int[] quickTip = GenerateLottoNumbers(6, 45); 
+                Console.WriteLine($"{i}. Quick-Tip: {FormatNumbers(quickTip)}");
+            }
+        }
+        static int[] GenerateLottoNumbers(int count, int maxNumber)
+        {
+            int[] numbers = new int[count]; 
+            Random rand = new Random();
+
+            for (int i = 0; i < count; i++)
+            {
+                int newNumber;
+                bool alreadyExists; 
+
+                do
+                {
+                    newNumber = rand.Next(1, maxNumber + 1); 
+                    alreadyExists = false; 
+
+
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (numbers[j] == newNumber)
+                        {
+                            alreadyExists = true; 
+                            break; 
+                        }
+                    }
+
+                } while (alreadyExists); 
+
+                numbers[i] = newNumber; 
+            }
+
+            Array.Sort(numbers); 
+            return numbers;
+        }
+
+        static string FormatNumbers(int[] numbers)
+        {
+            return string.Join(",", numbers);
+        }
+    }
+}
