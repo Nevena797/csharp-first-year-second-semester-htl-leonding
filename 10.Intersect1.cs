800ad103065e95412edca3dcf4d33430f1baa50a
Nevena <nevena.rogova@gmx.at>
2025-03-27 09:00:38 +0200

    Add files via upload

---
+++ 10.Intersect1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ArrayIntersection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersA = { 1, 3, 5, 7, 9 };
+            int[] numbersB = { 0, 1, 2, 3 };
+
+            int[] result = IntersectTools.Intersect(numbersA, numbersB);
+
+            Console.WriteLine("Intersection: " + string.Join(", ", result));
+        }
+    }
+
+    public static class IntersectTools
+    {
+        public static int[] Intersect(int[] array1, int[] array2)
+        {
+            return array1.Intersect(array2).ToArray();
+        }
+    }
+}
