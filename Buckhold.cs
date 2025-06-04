903b65e36a14318e6411b4505782bcfdce5b7214
Nevena <nevena.rogova@gmx.at>
2025-06-04 22:51:31 +0300

    Add files via upload

---
+++ Buckhold.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+
+namespace Buckhold
+{
+    internal class Program
+    {
+        static void WriteToCSV(Product[] prods, string filename)
+        {
+            string[] lines = new string[prods.Length + 1]; // +1 because of header
+            lines[0] = "ProductCode;Description;TaxClass;Retail"; // Заглавие като string
+
+            for (int i = 0; i < prods.Length; i++)
+            {
+                Product p = prods[i];
+                lines[i + 1] = $"{p.ProductCode};{p.Description};{p.TaxClass};{p.Retail}";
+            }
+
+            File.WriteAllLines(filename, lines);
+        }
+    }
+
+    public class Product
+    {
+        public int ProductCode { get; set; }
+        public string Description { get; set; }
+        public string TaxClass { get; set; }
+        public decimal Retail { get; set; }
+    }
+}
